b28f594f592bfd943d094937d6f35bc9f29d0969
Ubuntu <ubuntu@ip-172-31-41-71.ec2.internal>
2020-08-09 20:33:32 +0000

    everything in the newly setup theme-extension

---
+++ ckanext/example_theme/plugin.py
@@ -0,0 +1,16 @@
+# encoding: utf-8
+'''
+plugin.py
+'''
+import ckan.plugins as plugins
+import ckan.plugins.toolkit as toolkit
+
+
+class Example_ThemePlugin(plugins.SingletonPlugin):
+    plugins.implements(plugins.IConfigurer)
+    # IConfigurer
+
+    def update_config(self, config_):
+        toolkit.add_template_directory(config_, 'templates')
+        toolkit.add_public_directory(config_, 'public')
+        toolkit.add_resource('fanstatic', 'example_theme')
